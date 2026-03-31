f5cb4d8516397791e3b9b836e2f5ad3ab0d11ffb
marwenkhlifi15 <marwen.khlifi15@outlook.com>
2026-03-31 19:55:00 +0100

    feat: add search feature

--- server/routes/main.js
+++ server/routes/main.js
@@ -43,21 +43,62 @@ router.get('', async (req, res) => {
 
 });
 
-// router.get('', async (req, res) => {
-//     const locals = {
-//         title: "NodeJS Blog",
-//         description: "Simple Blog created with NodeJS, Express & MongoDB"
-//     };
+/**
+ * GET /
+ * Post :id
+*/
 
-//     try {
-//         const data = await Post.find();
-//         res.render('index', {locals, data}); 
-//     } catch (error) {
-//         console.log(error);
-//     }
+router.get('/post/:id', async (req, res) => {
+    try {
+      let slug = req.params.id;
+      const data = await Post.findById({ _id: slug });
+
+      const locals = {
+        title: data.title,
+        description: "Simple Blog created with NodeJS, Express & MongoDB"
+      };
+
+
+      res.render('post', {locals, data}); 
+    } catch (error) {
+      console.log(error);
+    }
     
     
-// });
+});
+
+/**
+ * POST /
+ * Post - searchTerm
+*/
+
+router.post('/search', async (req, res) => {
+    try {
+      const locals = {
+        title: "Search",
+        description: "Simple Blog created with NodeJS, Express & MongoDB"
+      };
+
+      let searchTerm = req.body.searchTerm;
+      const searchNoSpecialChar = searchTerm.replace(/[^a-zA-Z0-9 ]/g, "");
+
+      const data = await Post.find({
+      $or: [
+        { title: { $regex: new RegExp(searchNoSpecialChar, 'i') }},
+        { body: { $regex: new RegExp(searchNoSpecialChar, 'i') }}
+      ]
+    });
+      res.render('search', {
+        locals,
+        data
+      }); 
+    } catch (error) {
+      console.log(error);
+    }
+    
+    
+});
+
 
 router.get('/about', (req, res) => {
   res.render('about', {
@@ -123,3 +164,23 @@ module.exports = router;
 // }
 
 // insertPostData();
+
+
+// router.get('/post/:id', async (req, res) => {
+//     try {
+//       let slug = req.params.id;
+//       const data = await Post.findById({ _id: slug });
+
+//       const locals = {
+//         title: data.title,
+//         description: "Simple Blog created with NodeJS, Express & MongoDB"
+//       };
+
+
+//       res.render('post', {locals, data}); 
+//     } catch (error) {
+//       console.log(error);
+//     }
+    
+    
+// });
