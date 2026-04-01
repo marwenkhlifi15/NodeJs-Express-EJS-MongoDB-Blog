686d6d7cfaa54b7da918ac0cf549c78036d777b3
marwenkhlifi15 <marwen.khlifi15@outlook.com>
2026-04-01 10:49:08 +0100

    feat: add admin functionality and session management with cookies

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ require('dotenv').config();
 
 const express = require('express');
 const expressLayout = require('express-ejs-layouts');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const MongoConnect = require('connect-mongo');
 
-const connectDB = require('./server/config/db')
-
+const connectDB = require('./server/config/db');
 const app = express();
 const PORT = 5000 || process.env.PORT;
 
@@ -13,6 +15,16 @@ connectDB();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cookieParser());
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    store: MongoConnect.MongoStore.create({
+        mongoUrl: process.env.MONGODB_URI
+    }),
+    //cookie: { maxAge: new Date ( Date.now() + (3600000) ) } 
+}));
 
 app.use(express.static('public'));
 
@@ -22,6 +34,7 @@ app.set('layout', './layouts/main');
 app.set('view engine', 'ejs');
 
 app.use('/', require('./server/routes/main'));
+app.use('/', require('./server/routes/admin'));
 
 app.listen(PORT, () => {
     console.log(`App listening in port ${PORT}`);
